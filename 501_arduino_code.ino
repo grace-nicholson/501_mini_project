3356002f46e5ecf2e6addfa1301221089d62a37d
Grace Nicholson <nicholson.gf@live.co.uk>
2025-11-24 16:32:27 +0000

    Update 501_arduino_code.ino
    Updated WiFi credentials to placeholders.

--- 501_arduino_code.ino
+++ 501_arduino_code.ino
@@ -7,8 +7,8 @@ Sensor humid(SENSOR_ID_HUM);
 SensorBSEC bsec(SENSOR_ID_BSEC);
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "grace";        // your network SSID (name)
-char pass[] = "beanbean";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "XXX";        // your network SSID (name)
+char pass[] = "XXX";    // your network password (use for WPA, or use as key for WEP)
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
